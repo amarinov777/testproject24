e637cbb76fb69e1d830507c311ea94d080c68f61
Alex <marinov.alex.2007@mail.ru>
2025-12-28 13:51:09 +0200

    +bad comments

---
+++ testproject24/jmnbvcx.c
@@ -0,0 +1,22 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <limits.h>
+#include <float.h>
+#include <locale.h>
+
+void main(void)
+{
+	setlocale(LC_ALL, "RUS");
+	double x, y, z, b;
+	printf("\n ¬ведите х = ");
+	scanf_s("%lf", &x);
+	printf("\n ¬ведите y = ");
+	scanf_s("%lf", &y);
+	z = 2 * sin(3 * x + y);
+	printf("\n z = %lf", z);
+	b = bad comments / (y * (x - z) + log(fabs(x * z)));
+	printf("\n b = %lf", b);//bad comments
+	//bad comments
+	_getch();
+}
